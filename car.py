aa9bad0a0051651a97e8fa2a50559dc9424c583d
Mehul Williams <mehulwilliams@gmail.com>
2023-09-20 13:04:36 -0700

    Working Radar - very slow
    Next step is to reduce the render to 100x100 and create custom gym env

--- car.py
+++ car.py
@@ -5,7 +5,7 @@ import trail as tr
 WIDTH = 1440
 HEIGHT = 900
 
-ROAD_COLOR = (255, 255, 255)
+OFF_ROAD = (0, 0, 0)
 
 top_speed = 1000
 
@@ -15,6 +15,7 @@ accel_rate = .03
 coast_rate = .015
 brake_rate = .02
 
+pos_turn_rate = .025
 neg_turn_rate = .25
 
 
@@ -30,14 +31,16 @@ class Car(arc.Sprite):
         self.forward_x = 0
         self.forward_y = 0
         self.angle = 270
-
+        self.radar_poll = 0
+        self.radar_rays = 4
+        self.radar_list = [(0,0,0)] * self.radar_rays
+                           
         self.speed = 0.0
         self.turn_speed = 0
 
         self.horizontal_input = 0
         self.vertical_input = 0
 
-        self.pos_turn_rate = .025
         self.grip = 4
 
         self.gas = False
@@ -48,6 +51,18 @@ class Car(arc.Sprite):
         self.trail_renderer = tr.TrailRenderer()
         self.on_track = ()
 
+    def reset(self):
+        self.center_x = WIDTH/2
+        self.center_y = 800
+
+        self.move_force_x = 0
+        self.move_force_y = 0
+        self.forward_x = 0
+        self.forward_y = 0
+        self.angle = 270
+
+        self.speed = 0.0
+        self.turn_speed = 0
 
     def bound(self):
         if self.center_y > HEIGHT:
@@ -63,11 +78,11 @@ class Car(arc.Sprite):
     # format: [controller input] -/+= 0 if [controller input] >/<= [deadzone] else [nudge value]
     def processInput(self):
         if self.gas: #accel
-            self.pos_turn_rate = .03
+            pos_turn_rate = .03
             self.grip = 4
             self.vertical_input += 0 if self.vertical_input >= 1 else accel_rate
         else:
-            self.pos_turn_rate = .05
+            pos_turn_rate = .05
             self.grip = 5
         if not self.gas and self.vertical_input >= 0: # coasting
             self.vertical_input = 0 if self.vertical_input <= .1 else self.vertical_input - coast_rate
@@ -77,13 +92,66 @@ class Car(arc.Sprite):
             self.vertical_input = 0 if self.vertical_input >= -.1 else self.vertical_input + accel_rate
 
         if self.left_turn:
-            self.horizontal_input += 0 if self.horizontal_input >= 1 else self.pos_turn_rate
+            self.horizontal_input += 0 if self.horizontal_input >= 1 else pos_turn_rate
         if not self.left_turn and self.horizontal_input >= 0:
             self.horizontal_input = 0 if self.horizontal_input <= .1 else self.horizontal_input - neg_turn_rate
         if self.right_turn: 
-            self.horizontal_input -= 0 if self.horizontal_input <= -1 else self.pos_turn_rate
+            self.horizontal_input -= 0 if self.horizontal_input <= -1 else pos_turn_rate
         if not self.right_turn and self.horizontal_input < 0:
             self.horizontal_input = 0 if self.horizontal_input >= -.1 else self.horizontal_input + neg_turn_rate
+        
+    # polling_rate = n, where radar polls once every n frames
+    def update_radar_list(self, angle_rad, polling_rate=2, FOV=2.094396): #fov in radians
+        
+        angle = angle_rad - (FOV/2)
+        #angle = angle_rad - 1.047198
+        radar_range = 50
+
+        radar_steps = 7  #how many points to sample along ray.
+        if self.radar_poll == polling_rate-1:
+            self.radar_poll = 0
+            for i in range(len(self.radar_list)):
+                _x = math.sin(angle)
+                _y = math.cos(angle)
+                #radar_range = max(_x*radar_scale, _y*radar_scale)
+                
+                self.radar_list[i] = (radar_range, self.center_x - _x*radar_range, self.center_y + _y*radar_range)
+                
+                max_dist = arc.get_distance(self.center_x, self.center_y, self.center_x - _x*radar_range, self.center_y + _y*radar_range)
+                single_step = int(max_dist/radar_steps)
+                j = single_step
+                #print("max_dist:", max_dist, "j:", j)
+                #j *= 2
+                
+                while(j <= max_dist):
+                    stepped_x = self.center_x - (_x * j)
+                    stepped_y = self.center_y + (_y * j)
+                    if (i == 1):
+                        print(self.center_x, stepped_x, self.center_y, stepped_y)
+                    #self.on_track = True if arc.get_pixel(self.center_x - self.forward_x*12, self.center_y + self.forward_y*12, 3) == OFF_ROAD else False
+                    if (arc.get_pixel(stepped_x, stepped_y) == OFF_ROAD):
+                        dist = arc.get_distance(self.center_x, self.center_y, stepped_x, stepped_y)
+                        self.radar_list[i] = (dist, stepped_x, stepped_y)
+                        j += max_dist
+                    j += single_step
+                
+                #self.radar_list[i] = (max_dist, self.center_x - _x*radar_range, self.center_y + _y*radar_range)
+                
+
+                angle += (FOV/(self.radar_rays-1))  
+                #angle += 0.523599         #(0.523599 = 30 deg in rad)
+        else :
+            self.radar_poll += 1
+
+
+        '''if self.radar_poll == polling_rate-1:
+            self.radar_poll = 0
+            for i in range(5):
+                self.radar_list[i] = (self.center_x - math.sin(angle)*radar_range, self.center_y + math.cos(angle)*radar_range)
+                angle += 0.523599        #(0.523599 = 30 deg in rad)
+        else:
+            self.radar_poll += 1
+        '''
         
     def on_update(self, delta_time):
         print("\n------")
@@ -91,8 +159,14 @@ class Car(arc.Sprite):
         self.bound()
         self.processInput()
         
-        #self.on_track = True if arc.get_pixel(self.center_x - self.forward_x*12, self.center_y + self.forward_y*12, 3) == ROAD_COLOR else False
-
+        #self.on_track = True if arc.get_pixel(self.center_x - self.forward_x*12, self.center_y + self.forward_y*12, 3) == OFF_ROAD else False
+        
+        #if (arc.get_pixel(self.center_x - self.forward_x*12, self.center_y + self.forward_y*12, 3) == OFF_ROAD) :
+        #    self.on_track = True 
+            #self.reset()
+            #print("resetting")
+        
+    
         self.speed = math.sqrt(self.move_force_y**2 + self.move_force_x**2)
 
         self.move_force_x += (self.forward_x * top_speed * self.vertical_input * delta_time) if self.speed < top_speed else 0
@@ -105,6 +179,8 @@ class Car(arc.Sprite):
         angle_rad = math.radians(self.angle)
         self.forward_x = math.sin(angle_rad)
         self.forward_y = math.cos(angle_rad)
+
+        self.update_radar_list(angle_rad)
         
         self.center_x -= self.move_force_x * delta_time
         self.center_y += self.move_force_y * delta_time
@@ -117,7 +193,6 @@ class Car(arc.Sprite):
         trail_car.center_x = self.center_x
         trail_car.center_y = self.center_y
         trail_car.angle = self.angle
-
         self.trail_renderer.update_trail(trail_car)
 
     def drawTrail(self, other):
--- main.py
+++ main.py
@@ -34,9 +34,9 @@ class Game(arc.Window):
         self.player_list.draw()
 
         if debug:
-            arc.draw_text("fps: " + str(int(arc.get_fps())), WIDTH-650, HEIGHT-30, arc.color.RED, align="right", width=600)
-            arc.draw_text("self.player_car.pos_turn_rate " + str(self.player_car.pos_turn_rate), WIDTH-650, HEIGHT-50, arc.color.RED, align="right", width=600)
-            arc.draw_text("self.player_car.grip " + str(self.player_car.grip), WIDTH-650, HEIGHT-70, arc.color.RED, align="right", width=600)
+            arc.draw_text("fps: " + str(int(arc.get_fps())), WIDTH-650, HEIGHT-30, arc.color.YELLOW, align="right", width=600)
+            arc.draw_text("self.player_car.on_track " + str(self.player_car.on_track), WIDTH-650, HEIGHT-50, arc.color.YELLOW, align="right", width=600)
+            arc.draw_text("self.player_car.grip " + str(self.player_car.grip), WIDTH-650, HEIGHT-70, arc.color.YELLOW, align="right", width=600)
 
             aim_x = self.player_car.center_x - (self.player_car.move_force_x)/3
             aim_y = self.player_car.center_y + (self.player_car.move_force_y)/3
@@ -45,12 +45,10 @@ class Game(arc.Window):
             #aim_x = self.player_car.center_x - (self.player_car.forward_x*13)
             #aim_y = self.player_car.center_y + (self.player_car.forward_y*13)
             #arc.draw_point(aim_x, aim_y, arc.color.GREEN, 5)
+
+            for i in self.player_car.radar_list:
+                arc.draw_point(i[1], i[2], arc.color.YELLOW, 5)
             
-
-
-
-       
-
 
     def on_update(self, delta_time):
         self.player_list.on_update()
@@ -60,6 +58,8 @@ class Game(arc.Window):
     def on_key_press(self, symbol, modifiers):
         if symbol == arc.key.ESCAPE:
             arc.close_window()
+        if symbol == arc.key.R:
+            self.player_car.reset()
 
         if symbol == arc.key.W or symbol == arc.key.UP:
             self.player_car.gas = True
